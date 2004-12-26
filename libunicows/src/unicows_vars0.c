4c64a11c6b35da20af34ea49603e05c888d0e833
vaclavslavik <vaclavslavik@d6f24e2a-453b-44e1-bdfa-953d8fb96c1e>
2004-12-26 12:21:36 +0000

    added license+copyright+rcsid information to source files
    git-svn-id: https://svn.code.sf.net/p/libunicows/code/trunk@85 d6f24e2a-453b-44e1-bdfa-953d8fb96c1e

--- libunicows/src/unicows_vars0.c
+++ libunicows/src/unicows_vars0.c
@@ -1,10 +1,36 @@
+/*
+ * Copyright (c) 2001-2004 Vaclav Slavik
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE. 
+ * 
+ * $Id$
+ * 
+ */
 
 #include <windows.h>
 
-// This is MSLU variable used to override unicows.dll loading. It is in
-// separate file so that the linker can play dirty tricks with it. In
-// particular, if one of app's object files define this symbol, this object
-// file (stored in unicows.lib) is never used and so the variable is
-// initially non-NULL.
-// See http://tinyurl.com/5depf for explanation of linker's behaviour.
+/* 
+   This is MSLU variable used to override unicows.dll loading. It is in
+   separate file so that the linker can play dirty tricks with it. In
+   particular, if one of app's object files define this symbol, this object
+   file (stored in unicows.lib) is never used and so the variable is
+   initially non-NULL.
+   See http:  tinyurl.com/5depf for explanation of linker's behaviour.
+ */
 HMODULE (__stdcall *_PfnLoadUnicows)(void) = NULL;
